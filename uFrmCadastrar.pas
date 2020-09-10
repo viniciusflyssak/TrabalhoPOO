628f6b6d910dbf38db11aacd15ab81057cba73de
Vinicius Flyssak <63611482+viniciusflyssak@users.noreply.github.com>
2020-09-10 00:28:38 -0300

    Update uFrmCadastrar.pas

--- uFrmCadastrar.pas
+++ uFrmCadastrar.pas
@@ -62,6 +62,8 @@ begin
       Funcionario.Ganho := Funcionario.Salario + (Gestor.HoraExtra * 25)
     else
       Funcionario.Ganho := Funcionario.Salario + (Vendedor.Comissao * 0.15);
+Funcionarios[Contador] := Funcionario;
+end.
   ShowMessage('Total ganho: ' + FloatToStr(Funcionario.Ganho) + ' R$');end;
 Funcionarios[Contador] := Funcionario;
 end.
